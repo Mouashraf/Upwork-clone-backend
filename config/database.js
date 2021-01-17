7021c8b57748bd842eca1352641e6ec62ac0dbbb
AbdulrahmanRoshdy <AbdulrahmanRoshdy@outlook.com>
2021-01-17 22:17:12 +0200

    Add Saved collection into talent Schema and add proposing logic

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ class Connection {
   constructor() {
     const url =
       process.env.MONGODB_URI ||
-      `mongodb://localhost:27017/Upwork`;
+      `mongodb://localhost:27017/node-graduation-project`;
     console.log("Establish new connection with url", url);
     mongoose.Promise = global.Promise;
     mongoose.set("useNewUrlParser", true);
