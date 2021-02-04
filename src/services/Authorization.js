8e8c636c8c6a3af752da8c72ac3458e2b6c06451
AbdulrahmanRoshdy <AbdulrahmanRoshdy@outlook.com>
2021-02-04 23:42:34 +0200

    login update

--- src/services/Authorization.js
+++ src/services/Authorization.js
@@ -4,7 +4,7 @@ module.exports.authorize = (req, resp, next) => {
   const decoded = jwt.decode(req.cookies.jwt, "privateGP");
   console.log(decoded.Username);
   console.log(req.params.UserName);
-  if (decoded.Username == req.params.UserName) {
+  if (decoded.Username === req.params.UserName) {
     next();
   } else {
     return resp.status(401).json({
